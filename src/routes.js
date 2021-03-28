319d52be1d02baab1625e1f9e93ceae62a98fe7a
Cássia <cascrisbern@gmail.com>
2021-03-28 11:51:18 -0300

    Adding boilerplate code

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const routes = express.Router()
+
+const users = require("./controllers/users")
+
+module.exports = routes
+
+routes.get("/", users.index)
+routes.get("/search", users.search)
+routes.get("/users/:login", users.user)
