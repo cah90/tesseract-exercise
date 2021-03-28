ae2bb84973257da869d8df84ac2fcee25f40a72a
Cássia <cascrisbern@gmail.com>
2021-03-28 14:30:11 -0300

    the user search and the user detailed page add readme style for the user pages user view and user controller function

---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align="center">
+<br>
+  <img src="preview.png" alt="Preview of the index page of the website" width="400">
+<br>
+<br>
+Tesseract Challenge
+</h1>
+
+<p align="center">This is a challenge proposed by Tesseract.</p>
+
+<p align="center">
+  <a href="https://opensource.org/licenses/MIT">
+    <img src="https://img.shields.io/badge/License-MIT-blue.svg" alt="License MIT">
+  </a>
+</p>
+
+## Getting started
+
+After pulling the code in the folder, you
+should go to the folder using your terminal and do:
+
+````
+npm install
+````
+
+After downloading all the needed packages,
+do:
+
+````
+nodemon src/server.js
+````
+
+This will initialize the server and you can go to http://localhost:3000/
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://opensource.org/licenses/MIT) page for details.
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,14 @@
 const axios = require('axios')
 
+function formattedDate(date) {
+  const githubSubscriptionDate = new Date(date)
+  const day = githubSubscriptionDate.getDate()
+  const month = githubSubscriptionDate.getMonth() + 1
+  const year = githubSubscriptionDate.getFullYear()
+
+  return `${day}/${month}/${year}`
+}
+
 module.exports = {
   async index(req, res) {
     try {
@@ -9,4 +18,40 @@ module.exports = {
       console.log(err)
     }
   },
+
+  async search(req, res) {
+    try {
+      const { filter } = req.query
+
+      const { data } = await axios.get('https://api.github.com/orgs/grupotesseract/public_members')
+
+      let filteredUsers = []
+
+      data.map( user => {
+        if (user.login.toLowerCase().includes(filter.toLowerCase())) {
+          filteredUsers.push(user)
+        }
+      })
+
+      return res.render("allUsers", { users: filteredUsers })
+    } catch(err) {
+      console.log(err)
+    }
+  },
+
+  async user(req, res) {
+    try {
+      const { login } = req.params
+      const { data: user } = await axios.get(`https://api.github.com/users/${login}`)
+
+      if (user.login == login) {
+        user.created_at = formattedDate(user.created_at)
+        return res.render("user", { user })
+      } else {
+        return res.send("This user was not found.")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
 }
